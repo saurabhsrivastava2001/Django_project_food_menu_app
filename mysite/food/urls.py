ba6c155b6cd49bf20cde31ecfdac465e0a2fd786
saurabhsrivastava2001 <srisaurabh2001@gmail.com>
2025-06-22 02:26:11 +0530

    many changes: details , clickable, url

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,7 +1,10 @@
 from . import views
 from django.urls import path
+
+app_name='food'
 urlpatterns = [
     path('',views.index,name='index'),
     path('item/',views.item,name='item'),
     path('contact/',views.contact,name='item'),
+    path('<int:item_id>/',views.detail,name='detail')
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from .models import Item
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world I am saurabh!')
+    item_list= Item.objects.all()
+    context={
+        'item_list':item_list,
+    }
+    return render(request,'food/index.html',context)
 
 def item(request):
     return HttpResponse('this is my item!')
@@ -10,9 +15,9 @@ def item(request):
 def contact (request):
     return HttpResponse(' this is my contact page')
 
-
-
-
-
-
-
+def detail(request,item_id):
+    item= Item.objects.get(pk=item_id)
+    context={
+        'item':item,
+    }
+    return render(request,'food/detail.html',context)
